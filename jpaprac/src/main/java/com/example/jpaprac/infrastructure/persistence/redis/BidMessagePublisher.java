52b9d598c379e7769d929fa7be40473e207c9266
E-greene <phoex1106@gmail.com>
2025-07-03 14:28:55 +0900

    redis관련 코드 삭제

--- jpaprac/src/main/java/com/example/jpaprac/infrastructure/persistence/redis/BidMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpaprac.infrastructure.persistence.redis;
-
-import com.example.jpaprac.presentation.dto.bid.BidMessage;
-
-public interface BidMessagePublisher {
-    void publish(BidMessage message);
-}
